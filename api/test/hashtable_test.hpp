b27c11f79d142ad1d1b7a4a67b867bab86064482
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-02-18 10:00:08 -0800

    Refactoring test files to reduce redundance.
    Also makes the tests run faster.

--- api/test/hashtable_test.hpp
+++ api/test/hashtable_test.hpp
@@ -9,69 +9,17 @@
 #include "common/common.h"
 #include "hashtable/hashtable.h"
 
-#include "mock_device.hpp"
+#include "test_fixture.hpp"
 
-class HashTableFixture : public ::testing::Test {
-    public:
-        static const size_t NBLK;
-        static const size_t BLK_SZ;
-        static MockDevice device;
-
-        static int mock_get_dev_info(device_info_t* di) {
-            di->di_size_blocks = NBLK;
-            di->di_block_size  = BLK_SZ;
-            return 0;
-        }
-
-        static ssize_t mock_alloc_metadata(size_t nblocks, paddr_t *blk) {
-            *blk = device.allocate(nblocks);
-            return (ssize_t)(nblocks);
-        }
-
-        static ssize_t mock_dealloc_metadata(size_t nblocks, paddr_t blk) {
-            device.deallocate(blk, nblocks);
-            return (ssize_t)(nblocks);
-        }
-
-        static ssize_t mock_alloc_data(size_t nblocks, paddr_t *blk) {
-            return mock_alloc_metadata(nblocks, blk);
-        }
-
-        static ssize_t mock_dealloc_data(size_t nblocks, paddr_t blk) {
-            return mock_dealloc_metadata(nblocks, blk);
-        }
-
-        static ssize_t mock_read(paddr_t blk, off_t off, size_t nbytes, char* buf) {
-            return device[blk].read(off, nbytes, buf);
-        }
-
-        static ssize_t mock_write(paddr_t blk, off_t off, size_t nbytes, const char* buf) {
-            return device[blk].write(off, nbytes, buf);
-        }
-
+class HashTableFixture : public TestFixture {
     protected:
         void SetUp() override {
-           idx_spec.idx_mem_man            = new mem_man_fns_t;
-           idx_spec.idx_mem_man->mm_malloc = malloc;
-           idx_spec.idx_mem_man->mm_free   = free;
-
-           idx_spec.idx_callbacks                      = new callback_fns_t;
-           idx_spec.idx_callbacks->cb_write            = mock_write;
-           idx_spec.idx_callbacks->cb_read             = mock_read;
-           idx_spec.idx_callbacks->cb_alloc_metadata   = mock_alloc_metadata;
-           idx_spec.idx_callbacks->cb_alloc_data       = mock_alloc_data;
-           idx_spec.idx_callbacks->cb_dealloc_metadata = mock_dealloc_metadata;
-           idx_spec.idx_callbacks->cb_dealloc_data     = mock_dealloc_data;
-           idx_spec.idx_callbacks->cb_get_dev_info     = mock_get_dev_info;
+            TestFixture::SetUp();
+            init_err = hashtable_initialize(&idx_spec, &hashtable, &metadata_loc);
         }
 
-        void TearDown() override {
-
-           delete idx_spec.idx_mem_man;
-           delete idx_spec.idx_callbacks;
-        }
-
-
-        idx_spec_t idx_spec;
+        idx_struct_t hashtable = {0,};
+        paddr_t metadata_loc = 0;
+        int init_err;
 };
 
---
+++ api/test/test_fixture.cpp
@@ -0,0 +1,58 @@
+#include "test_fixture.hpp"
+
+const size_t TestFixture::NBLK   = 1024 * 1024;
+const size_t TestFixture::BLK_SZ = 4096;
+MockDevice TestFixture::device = MockDevice(TestFixture::NBLK,
+                                            TestFixture::BLK_SZ);
+
+int TestFixture::mock_get_dev_info(device_info_t* di) {
+    di->di_size_blocks = NBLK;
+    di->di_block_size  = BLK_SZ;
+    return 0;
+}
+
+ssize_t TestFixture::mock_alloc_metadata(size_t nblocks, paddr_t *blk) {
+    *blk = device.allocate(nblocks);
+    return (ssize_t)(nblocks);
+}
+
+ssize_t TestFixture::mock_dealloc_metadata(size_t nblocks, paddr_t blk) {
+    device.deallocate(blk, nblocks);
+    return (ssize_t)(nblocks);
+}
+
+ssize_t TestFixture::mock_alloc_data(size_t nblocks, paddr_t *blk) {
+    return mock_alloc_metadata(nblocks, blk);
+}
+
+ssize_t TestFixture::mock_dealloc_data(size_t nblocks, paddr_t blk) {
+    return mock_dealloc_metadata(nblocks, blk);
+}
+
+ssize_t TestFixture::mock_read(paddr_t blk, off_t off, size_t nbytes, char* buf) {
+    return device[blk].read(off, nbytes, buf);
+}
+
+ssize_t TestFixture::mock_write(paddr_t blk, off_t off, size_t nbytes, const char* buf) {
+    return device[blk].write(off, nbytes, buf);
+}
+
+void TestFixture::SetUp() {
+   idx_spec.idx_mem_man            = new mem_man_fns_t;
+   idx_spec.idx_mem_man->mm_malloc = malloc;
+   idx_spec.idx_mem_man->mm_free   = free;
+
+   idx_spec.idx_callbacks                      = new callback_fns_t;
+   idx_spec.idx_callbacks->cb_write            = mock_write;
+   idx_spec.idx_callbacks->cb_read             = mock_read;
+   idx_spec.idx_callbacks->cb_alloc_metadata   = mock_alloc_metadata;
+   idx_spec.idx_callbacks->cb_alloc_data       = mock_alloc_data;
+   idx_spec.idx_callbacks->cb_dealloc_metadata = mock_dealloc_metadata;
+   idx_spec.idx_callbacks->cb_dealloc_data     = mock_dealloc_data;
+   idx_spec.idx_callbacks->cb_get_dev_info     = mock_get_dev_info;
+}
+
+void TestFixture::TearDown() {
+   delete idx_spec.idx_mem_man;
+   delete idx_spec.idx_callbacks;
+}
