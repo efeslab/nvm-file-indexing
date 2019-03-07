96aca23e3d53e8372808ca0a4c3f96557aa1d6c5
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-03-07 13:34:35 -0800

    Starting radix tree dev.

---
+++ api/radixtree/hash_functions.h
@@ -0,0 +1,25 @@
+#ifndef __NVM_IDX_HASH_FUNCTIONS_H__
+#define __NVM_IDX_HASH_FUNCTIONS_H__
+
+#include <stdint.h>
+
+#include "common/common.h"
+
+typedef laddr_t (*hash_func_t)(paddr_t key);
+
+// https://gist.github.com/badboy/6267743
+static inline laddr_t nvm_idx_hash6432shift(paddr_t key) {
+  key = (~key) + (key << 18); // key = (key << 18) - key - 1;
+  key = key ^ (key >> 31);
+  key = key * 21; // key = (key + (key << 2)) + (key << 4);
+  key = key ^ (key >> 11);
+  key = key + (key << 6);
+  key = key ^ (key >> 22);
+  return (laddr_t) key;
+}
+
+static inline laddr_t nvm_idx_direct_hash (paddr_t v) {
+  return (laddr_t)(v & 0xFFFFFFFF);
+}
+
+#endif  //__HASH_FUNCTIONS_H__
