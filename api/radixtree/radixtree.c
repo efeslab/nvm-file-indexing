9fc8ff703d2cdb6f6df4a4989e51a906ac038ee7
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-03-14 12:59:36 -0700

    Radix tree init works!
    Now to see if create will work.
    
    Note to self: malloc is expensive.

--- api/radixtree/radixtree.c
+++ api/radixtree/radixtree.c
@@ -1,5 +1,266 @@
 #include "radixtree.h"
 
-int radixtree_init(const idx_spec_t*, const paddr_range_t*, idx_struct_t*) {
-    dev
+#include <math.h>
+
+#define PRESENT_BIT (1llu << 63)
+_Static_assert(PRESENT_BIT != 0llu, "Too much shift!");
+#define ENT_IS_PRESENT(e) ((e) & PRESENT_BIT)
+#define ENT_PADDR(e) ((e) & ~PRESENT_BIT)
+
+#define MKADDR(i, l) ( ((paddr_t) i) << 32 | ((paddr_t)l) )
+
+#define GET_RADIX(i) radixtree_meta_t *radix = (radixtree_meta_t*)(i)->idx_metadata
+
+static int write_page(radixtree_meta_t *radix, paddr_t page, paddr_t *cache_page) {
+    ssize_t ret = CB(radix, cb_write,
+                     page, 0, radix->node_nbytes, (char*)cache_page);
+
+    if (ret != radix->node_nbytes) return -EIO;
+    return 0;
 }
+
+static int read_page(radixtree_meta_t *radix, paddr_t page, paddr_t *cache_page) {
+    ssize_t ret = CB(radix, cb_read,
+                     page, 0, radix->node_nbytes, (char*)cache_page);
+
+    if (ret != radix->node_nbytes) return -EIO;
+    return 0;
+}
+
+// Returns -EIO if could not read, 0 if no metadata, 1 if metadata.
+static int read_metadata(radixtree_meta_t *radix) {
+    ondev_radix_meta_t ondev;
+    ssize_t ret = CB(radix, cb_read,
+                     radix->metadata_blk, 0, sizeof(ondev), (char*)&ondev);
+
+    if (ret != sizeof(ondev)) return -EIO;
+    if (ondev.magic == RADIX_MAGIC) {
+        radix->top_page = ondev.top_page;
+        return 1;
+    }
+    return 0;
+}
+
+// Returns 0 on success, or -errno otherwise.
+static int write_metadata(radixtree_meta_t *radix) {
+    ondev_radix_meta_t ondev = {
+        .magic    = RADIX_MAGIC,
+        .top_page = radix->top_page
+    };
+
+    ssize_t ret = CB(radix, cb_write,
+                     radix->metadata_blk, 0, sizeof(ondev), (char*)&ondev);
+
+    if (ret != sizeof(ondev)) return -EIO;
+    return 0;
+}
+
+static int create_radix_node(radixtree_meta_t *radix, paddr_t page, 
+                             int depth, radix_node_t *node);
+
+static int read_radix_node(radixtree_meta_t *radix, radix_node_t *node) {
+    int err = read_page(radix, node->rn_page, node->rn_dev_contents);
+    if (err) return err;
+
+    for (size_t i = 0; i < radix->ents_per_node; ++i) {
+        radix_node_t *n = &(node->rn_cache_tree[i]);
+
+        paddr_t entry = node->rn_dev_contents[i];
+        int depth = node->rn_depth + 1;
+        if (!n) {
+            err = create_radix_node(radix, entry, depth, n);
+            if (err) return err;
+        } else {
+            n->rn_depth = depth;
+            n->rn_page  = entry;
+        }
+    }
+
+    return err;
+}
+
+static int create_radix_node(radixtree_meta_t *radix, paddr_t page, 
+                             int depth, radix_node_t *node) 
+{
+    if_then_panic(!node, "Must be preallocated!\n");
+
+    node->rn_page = page;
+    node->rn_depth = depth;
+    node->rn_dev_contents = ZALLOC(radix, radix->node_nbytes);
+    node->rn_cache_tree = ZALLOC(radix, radix->ents_per_node * sizeof(*node));
+    if (!node->rn_dev_contents || !node->rn_cache_tree) return -ENOMEM;
+
+    if (page > 0) {
+        return read_radix_node(radix, node);
+    }
+
+    return 0;
+}
+
+static int write_radix_node(radixtree_meta_t *radix, radix_node_t *node) {
+    int ret = write_page(radix, node->rn_page, node->rn_dev_contents);
+    if (ret) return ret;
+}
+
+static radix_node_t* index_node(radixtree_meta_t *radix, 
+                                radix_node_t *node, size_t idx) 
+{
+    if_then_panic(idx >= radix->ents_per_node, 
+                  "Bad index calculation! (%llu)\n", idx);
+    if_then_panic(!node, "Can't index nullptr!\n");
+
+    radix_node_t *child = &(node->rn_cache_tree[idx]);
+
+    if (!child) {
+        paddr_t entry = node->rn_dev_contents[idx];
+        if (!entry) {
+            // We need to allocate this page!
+            ssize_t nalloc = CB(radix, cb_alloc_metadata,
+                                radix->nblk_per_node, &entry);
+            if_then_panic(nalloc != radix->nblk_per_node, "Could not allocate!");
+
+            node->rn_dev_contents[idx] = entry;
+            node->rn_cache_tree[idx].rn_page = entry;
+            node->rn_cache_tree[idx].rn_cache_state = -1;
+            int ret = write_page(radix, node->rn_page, node->rn_dev_contents);
+            if (ret) return NULL;
+        }
+        int err = create_radix_node(radix, entry, node->rn_depth + 1, child);
+        if (err) return NULL;
+    }
+
+    return child;
+}
+
+static int insert_entry(radixtree_meta_t *radix, radix_node_t *node, 
+                        size_t idx, paddr_t entry)
+{
+    if_then_panic(node->rn_depth != radix->max_depth, 
+                  "Don't use this for data blocks!\n");
+
+    node->rn_dev_contents[idx] = entry;
+    node->rn_cache_state = 1;
+    return write_radix_node(radix, node);
+}
+
+static int lookup_entry(radixtree_meta_t *radix, radix_node_t *node, 
+                        size_t idx, paddr_t *entry)
+{
+    if_then_panic(node->rn_depth != radix->max_depth, 
+                  "Don't use this for data blocks!\n");
+
+    // TODO read!
+    *entry = node->rn_dev_contents[idx];
+    return 0;
+}
+
+
+/*******************************************************************************
+ * Section: Public API Functions.
+ ******************************************************************************/
+int radixtree_init(const idx_spec_t* idx_spec, idx_struct_t* idx_struct, 
+                   paddr_t *metadata_location) {
+
+    if_then_panic(!*metadata_location, "Nowhere to put metadata!\n");
+    
+    radixtree_meta_t *radix = ZALLOC(idx_spec, sizeof(*radix));
+    if_then_panic(!radix, "Could not allocate in-memory structure!\n");
+
+    device_info_t di;
+    int err = CB(idx_spec, cb_get_dev_info, &di);
+    if_then_panic(err, "Could not get device size!");
+
+    radix->blksz         = di.di_block_size;
+    radix->ent_size      = sizeof(paddr_t);
+    radix->ents_per_node = 1 << 16;
+    radix->max_depth     = 4;
+    radix->node_nbytes   = radix->ents_per_node * radix->ent_size;
+    radix->nblk_per_node = radix->node_nbytes / radix->blksz;
+    radix->ent_idx_mask  = radix->ents_per_node - 1;
+    radix->ent_shift     = (size_t)log2((double)radix->ents_per_node);
+    radix->metadata_blk  = *metadata_location; 
+    radix->idx_callbacks = idx_spec->idx_callbacks;
+    radix->idx_mem_man   = idx_spec->idx_mem_man; 
+
+    // Check status of metadata.
+    int merr = read_metadata(radix);
+    if (merr < 0) return merr;
+
+    if (!merr) {
+        // Need to first allocate the top page.
+        ssize_t nalloc = CB(radix, cb_alloc_metadata, 
+                            radix->nblk_per_node, &(radix->top_page));
+        if (nalloc != radix->nblk_per_node) return -ENOMEM;
+        int werr = write_metadata(radix);
+        if (werr) return werr;
+    }
+
+    // Read the top page.
+    radix->cached_tree = ZALLOC(radix, sizeof(*radix->cached_tree));
+    if (!radix->cached_tree) return -ENOMEM;
+    err = create_radix_node(radix, radix->top_page, 1, radix->cached_tree);
+    if (err) return err;
+
+    // If all of that is successful, finally set up structure.
+    idx_struct->idx_metadata  = (void*)radix;
+    idx_struct->idx_callbacks = idx_spec->idx_callbacks;
+    idx_struct->idx_mem_man   = idx_spec->idx_mem_man;
+    idx_struct->idx_fns       = &radixtree_fns;
+
+    return 0;
+}
+
+ssize_t radixtree_lookup(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         paddr_t *paddr) 
+{
+    return -ENOENT;
+}
+
+ssize_t radixtree_create(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         size_t npages, paddr_t *paddr)
+{
+    GET_RADIX(idx_struct);
+    // First two levels are just based on the inum.
+    size_t l1 = ((size_t)inum) >> radix->ent_shift;
+    size_t l2 = ((size_t)inum) & radix->ent_idx_mask;
+
+    radix_node_t *l2_node = index_node(radix, radix->cached_tree, l1);
+    radix_node_t *l3_node = index_node(radix, l2_node, l2);
+
+    // Do block allocation
+    ssize_t nalloc = CB(radix, cb_alloc_data, npages, paddr);
+    if (nalloc == 0) return -ENOMEM;
+    if (nalloc < 0) return nalloc;
+
+    // The following will change as we iterate.
+    size_t l3_current = laddr >> radix->ent_shift;
+    radix_node_t *l4_node = index_node(radix, l3_node, l3_current);
+
+    for (laddr_t l = 0; l < nalloc; ++l) {
+        size_t l3 = (laddr + l) >> radix->ent_shift;
+        size_t l4 = (laddr + l) & radix->ent_idx_mask;
+        if (l3 != l3_current) {
+            l3_current = l3;
+            l4_node = index_node(radix, l3_node, l3_current);
+        }
+
+        int err = insert_entry(radix, l4_node, l4, *paddr + (size_t)l);
+        if (err) return err;
+    }
+
+    return nalloc;
+}
+
+ssize_t radixtree_remove(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         size_t npages) 
+{
+    return -ENOENT;
+}
+
+idx_fns_t radixtree_fns = {
+    .im_init          = radixtree_init,
+    .im_init_prealloc = NULL,
+    .im_lookup        = radixtree_lookup,
+    .im_create        = radixtree_create,
+    .im_remove        = radixtree_remove
+};
--- api/radixtree/radixtree.h
+++ api/radixtree/radixtree.h
@@ -5,21 +5,66 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "common/common.h"
+
+typedef struct radix_node {
+    // Where this page is located on the device.
+    // First block of many.
+    paddr_t rn_page;
+    // How deep the node is in the tree. Radix tree has fixed depth.
+    size_t rn_depth;
+    // The contents of this page on the device.
+    paddr_t *rn_dev_contents;
+    // Status of cache: 0, uptodate, 1, dirty, -1, invalid
+    int rn_cache_state;
+    // The in-memory cached structures.
+    struct radix_node *rn_cache_tree;
+} radix_node_t;
+
+#define RADIX_MAGIC 0xfeedbeef
+typedef struct ondevice_radixtree_metadata {
+    uint32_t magic;
+    paddr_t top_page;
+} ondev_radix_meta_t;
+
 typedef struct radixtree_metadata {
     bool cached;
-    paddr_t top_level_page;
-    paddr_t *cached_tree;
+    paddr_t metadata_blk;
+    paddr_t top_page;
+    radix_node_t *cached_tree;
 
+    size_t blksz;
+    size_t ent_size;
+    // We can make the size of radix nodes larger than one block.
+    // We want 4 levels, so we have 16 bits of indexing per node.
+    size_t ents_per_node;
+    size_t node_nbytes;
+    size_t nblk_per_node;
+    size_t ent_idx_mask;
+    size_t ent_shift;
+
+    size_t max_depth;
+
+    callback_fns_t *idx_callbacks;
+    mem_man_fns_t  *idx_mem_man;
 
 } radixtree_meta_t;
 
-int radixtree_init(const idx_spec_t*, const paddr_range_t*, idx_struct_t*);
+int radixtree_init(const idx_spec_t *idx_spec, idx_struct_t *idx_struct, 
+                   paddr_t *metadata_location);
 
-ssize_t radixtree_lookup(idx_struct_t*, inum_t, laddr_t, paddr_t*);
+ssize_t radixtree_lookup(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         paddr_t *paddr);
 
-ssize_t radixtree_create(idx_struct_t*, inum_t, laddr_t, size_t, paddr_t*);
+ssize_t radixtree_create(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         size_t npages, paddr_t *paddr);
 
-ssize_t radixtree_remove(idx_struct_t*, inum_t, laddr_t, size_t);
+ssize_t radixtree_remove(idx_struct_t *idx_struct, inum_t inum, laddr_t laddr, 
+                         size_t npages);
+
+extern idx_fns_t radixtree_fns;
 
 #ifdef __cplusplus
 }
