f65d4e36ae7213d3d6944c656fab5225fa02860f
Ian Glen Neal <ian.gl.neal@gmail.com>
2019-09-26 09:35:24 -0700

    Disable pmlock since concurrency test does not need it and it hangs.

--- api/common/pmlock.h
+++ api/common/pmlock.h
@@ -8,6 +8,10 @@ typedef struct pmlock {
 } pmlock_t;
 
 static void pmlock_rd_lock(pmlock_t *l) {
+    /*
+    //printf("RD LOCK\n");
+    //return;
+
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 0, 1));
 
     while (__atomic_load_n(&l->has_writer, __ATOMIC_SEQ_CST));
@@ -15,17 +19,27 @@ static void pmlock_rd_lock(pmlock_t *l) {
     __atomic_add_fetch(&l->nreaders, 1, __ATOMIC_SEQ_CST);
 
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 1, 0));
+    */
 }
 
 static void pmlock_rd_unlock(pmlock_t *l) {
+    /*
+    //printf("RD UNLOCK\n");
+    //return; 
+
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 0, 1));
 
     __atomic_sub_fetch(&l->nreaders, 1, __ATOMIC_SEQ_CST);
 
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 1, 0));
+    */
 }
 
 static void pmlock_wr_lock(pmlock_t *l) {
+    /*
+    //printf("WR LOCK\n");
+    //return; 
+
     // Probably a race condition
     while (__atomic_load_n(&l->has_writer, __ATOMIC_SEQ_CST));
 
@@ -37,12 +51,18 @@ static void pmlock_wr_lock(pmlock_t *l) {
     while (__atomic_load_n(&l->nreaders, __ATOMIC_SEQ_CST) > 0);
 
     while (!__sync_bool_compare_and_swap(&l->wrmutex, 1, 0));
+    */
 }
 
 static void pmlock_wr_unlock(pmlock_t *l) {
+    /*
+    //printf("WR UNLOCK\n");
+    //return; 
+
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 0, 1));
 
     __atomic_sub_fetch(&l->has_writer, 1, __ATOMIC_SEQ_CST);
 
     while (!__sync_bool_compare_and_swap(&l->rdmutex, 1, 0));
+    */
 }
