3086de4c58b1e5ff5f5c6f8f3f765f1d374143f7
Tanvir Ahmed Khan <takh@umich.edu>
2020-04-25 23:15:13 +0000

    Added the modified run script for rsync benchmark

---
+++ benchmarks/rsync/run.sh
@@ -0,0 +1,17 @@
+cur_dir=`readlink -f ./`
+rsync_dir=$cur_dir/src
+workload_dir=$cur_dir/workload
+pmem_dir=/mnt/pmem-fsdax0/takh
+
+ulimit -c unlimited
+
+rm -rf $pmem_dir/*
+
+cp -r $workload_dir/src $pmem_dir && sync
+cd $pmem_dir
+mkdir dest
+( time $rsync_dir/rsync -Wr ./src dest/ ) 2>&1 | tee $cur_dir/results_time.txt
+
+rm -rf $pmem_dir/*
+
+cd $current_dir
