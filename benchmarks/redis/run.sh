2174eb2023f6452e2e39579853e8a516f70e1a2b
Tanvir Ahmed Khan <takh@umich.edu>
2020-04-30 00:26:42 +0000

    Fixed a bug in run script of redis

--- benchmarks/redis/run.sh
+++ benchmarks/redis/run.sh
@@ -3,13 +3,14 @@ output_dir=/home/takh/git-repos/nvm-file-indexing/benchmarks/redis
 flame_path=/home/takh/git-repos/nvm-file-indexing/benchmarks/FlameGraph
 for i in a b c d e f;
 do
-  src/redis-server redis.conf &
-  server_pid=$!
+  rm -rf /mnt/pmem-fsdax0/takh/redis/*
+  src/redis-server redis.conf --daemonize yes #&>/dev/null & disown; #&
+  server_pid=$(pgrep redis) #$!
   dirs -c
   pushd .
   cd $YCSB_PATH
-  $YCSB_PATH/bin/ycsb load redis -s -P $YCSB_PATH/workloads/workload$i -p "redis-host=127.0.0.1" -p "redis.port=6379"
-  perf record -o /tmp/perf.data -g -p $server_pid -- $YCSB_PATH/bin/ycsb run redis -s -P $YCSB_PATH/workloads/workload$i -p "redis-host=127.0.0.1" -p "redis.port=6379"
+  ./bin/ycsb load redis -s -P workloads/workload$i -p "redis.host=127.0.0.1" -p "redis.port=6379"
+  perf record -o /tmp/perf.data -g -p $server_pid -- ./bin/ycsb run redis -s -P workloads/workload$i -p "redis.host=127.0.0.1" -p "redis.port=6379"
   popd
   kill -SIGINT $server_pid
   # kill -9 $server_pid
